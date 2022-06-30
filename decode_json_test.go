7e86508c1a0b02ea857fb1d65ee046c0de2a7dc4
dyaksaa <diasnour0395@gmail.com>
2022-06-30 10:44:59 +0700

    json decode

--- decode_json_test.go
+++ decode_json_test.go
@@ -1,1 +1,21 @@
 package go_json
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+func TestDecodeJSON(t *testing.T) {
+	jsonstr := `{"FirstName": "Dyaksa", "MiddleName": "Jauharuddin", "LastName": "Nour", "Age": 27, "Married": false}`
+	jsonEncode := []byte(jsonstr)
+
+	customer := &User{}
+
+	err := json.Unmarshal(jsonEncode, customer)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(customer)
+}
